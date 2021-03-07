372b9739ea9fcba1eda5571021986d438258c534
svayalk <32828154+Rekjal@users.noreply.github.com>
2021-03-07 12:42:02 -0800

    add test for vendor constructor

--- VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -15,12 +15,27 @@ namespace VendorAndOrderTracker.Tests
     //   Order.ClearAll();
     // }
 
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test Order", "Crossant order", 35);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
     // [TestMethod]
-    // public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    // public void GetDescription_ReturnsDescription_String()
     // {
-    //   Order newOrder = new Order("test");
-    //   Assert.AreEqual(typeof(Order), newOrder.GetType());
+    //   //Arrange
+    //   string description = "Walk the dog.";
+
+    //   //Act
+    //   Item newItem = new Item(description);
+    //   string result = newItem.Description;
+
+    //   //Assert
+    //   Assert.AreEqual(description, result);
     // }
+
 
   }
 }
